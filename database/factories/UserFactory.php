508e27ea6586a4eb4de08b97141f797a7f4d5920
belfer24 <robert@DESKTOP-VG9FFEJ.localdomain>
2023-12-01 11:00:06 -0500

    spacing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'firstname' => $faker->name(),
         'address' => $faker->streetAddress(),
         'postal_code' => $faker->postcode(),
-        'city' => $faker->city(),
+        'city' => $faker -> city(),
         'country' => $faker->state(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10)
